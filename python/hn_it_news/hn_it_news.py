11c14c8b6e999b647a2c8b90f0c75d258d1e9e23
hndung98 <54524235+hndung98@users.noreply.github.com>
2025-08-17 21:33:08 +0700

    update hn_it_news.py

--- python/hn_it_news/hn_it_news.py
+++ python/hn_it_news/hn_it_news.py
@@ -1,7 +1,8 @@
 import os
 import requests
 import re
-from datetime import datetime, timedelta
+import sys
+from datetime import datetime, timedelta, timezone
 
 KEYWORDS = [
     "python",
@@ -19,8 +20,12 @@ KEYWORDS = [
     "nestjs",
 ]
 MAX_STORIES = 50
+OUTPUT_FOLDER = "./downloads"
 OUTPUT_FILE = "hn_it_news.txt"
+OUTPUT_FILE_PATH = OUTPUT_FOLDER + "/" + OUTPUT_FILE
 TIMEZONE_OFFSET = 7  # GMT+7
+
+bar_length = 30
 
 
 def get_story_ids(story_type="top"):
@@ -43,23 +48,30 @@ def matches_keywords(title):
 
 
 def main():
-    if os.path.exists("hn_it_news.txt"):
-        os.remove("hn_it_news.txt")
-        print("✅ Deleted hn_it_news.txt")
-    else:
-        print("⚠ hn_it_news.txt does not exist")
-    print("🔍 Getting Hacker News...")
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    now = datetime.now()
+    datetime_str = now.strftime("%Y%m%d_%H%M%S")
+    file_name_using_datetime = OUTPUT_FILE.replace(".txt", f"_{datetime_str}.txt")
+    output_file_path = OUTPUT_FOLDER + "/" + file_name_using_datetime
+
+    # if os.path.exists(OUTPUT_FILE_PATH):
+    #     os.remove(OUTPUT_FILE_PATH)
+    #     print(f"✅ Deleted the old {OUTPUT_FILE_PATH}")
+    # else:
+    #     print(f"⚠ {OUTPUT_FILE_PATH} does not exist")
+
+    print("\n🔍 Getting Hacker News...")
 
     story_ids = get_story_ids("top")[:MAX_STORIES] + get_story_ids("new")[:MAX_STORIES]
+    length = len(story_ids)
     results = []
 
-    for sid in story_ids:
-        print(sid)
+    for i, sid in enumerate(story_ids, start=1):
         story = get_story_detail(sid)
         if story and "title" in story and matches_keywords(story["title"]):
-            story_time = datetime.utcfromtimestamp(story.get("time", 0)) + timedelta(
-                hours=TIMEZONE_OFFSET
-            )
+            story_time = datetime.fromtimestamp(
+                story.get("time", 0), timezone.utc
+            ) + timedelta(hours=TIMEZONE_OFFSET)
             results.append(
                 {
                     "title": story["title"],
@@ -70,15 +82,20 @@ def main():
                     "time": story_time.strftime("%Y-%m-%d %H:%M:%S"),
                 }
             )
+        percent = int(i / length * 100)
+        filled = int(bar_length * i / length)
+        bar = "█" * filled + "-" * (bar_length - filled)
+        sys.stdout.write(f"\r|{bar}| {percent}% ({i}/{length})")
+        sys.stdout.flush()
 
-    with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    with open(output_file_path, "w", encoding="utf-8") as f:
         f.write(
-            f"Hacker News IT Stories (Top + New) - {datetime.utcnow() + timedelta(hours=TIMEZONE_OFFSET)} GMT+7\n\n"
+            f"Hacker News IT Stories (Top + New) - {datetime.now() + timedelta(hours=TIMEZONE_OFFSET)} GMT+7\n\n"
         )
         for r in results:
             f.write(f"{r['time']} | {r['title']} (Score: {r['score']})\n{r['url']}\n\n")
 
-    print(f"✅ Saved {len(results)} at {OUTPUT_FILE}")
+    print(f"\n\n✅ Saved {len(results)} at {output_file_path}")
 
 
 if __name__ == "__main__":
